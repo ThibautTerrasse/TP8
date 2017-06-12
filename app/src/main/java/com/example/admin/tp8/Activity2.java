6270f6e07ea8aad73ee18bcc9d52f4af2ab9f281
Thibaut Terrasse <thibautterrasse@hotmail.fr>
2017-06-12 18:52:26 +0200

    boucle de question

--- app/src/main/java/com/example/admin/tp8/Activity2.java
+++ app/src/main/java/com/example/admin/tp8/Activity2.java
@@ -7,10 +7,7 @@ import android.widget.TextView;
 
 public class Activity2 extends AppCompatActivity {
 
-    private VraiFaux[] question = new VraiFaux[]{
-            new VraiFaux("Qui a fait a pole position au grandprix de F1 du Cananda?", " Vrai, et il a réalisé le record de la piste!","Faux, il a fait le second chrono", true),
-            new VraiFaux("Qui a gagné le grandprix de F1 du Cananda?", " Vrai et c'est sa 56ème victoire en carriere!","Faux, il a fini troisième", true),
-    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +16,10 @@ public class Activity2 extends AppCompatActivity {
 
         //on récupère l'intent qui a lancé votre activité
         Intent intent = getIntent();
-        String texte = intent.getStringExtra("param");
+       int texte = intent.getIntExtra("param",0);
 
         TextView editText = (TextView) findViewById(R.id.response);
-        editText.setText(texte);
+        editText.setText(texte + "");
 
     }
 }
--- app/src/main/java/com/example/admin/tp8/MainActivity.java
+++ app/src/main/java/com/example/admin/tp8/MainActivity.java
@@ -6,29 +6,88 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private VraiFaux[] question = new VraiFaux[]{
+            new VraiFaux("Qui a fait a pole position au grandprix de F1 du Cananda?", "Lewis Hamilton","Sebastian Vettel", true),
+            new VraiFaux("Qui a gagné le grandprix de F1 du Cananda?", "Lewis Hamilton","Daniel Ricciardo", true),
+            new VraiFaux("Qui a fait a pole position au grandprix de F1 du Cananda?", "Lewis Hamilton","Sebastian Vettel", true),
+            new VraiFaux("Qui a gagné le grandprix de F1 du Cananda?", "Lewis Hamilton","Daniel Ricciardo", true),
+            new VraiFaux("Qui a fait a pole position au grandprix de F1 du Cananda?", "Lewis Hamilton","Sebastian Vettel", true),
+            new VraiFaux("Qui a gagné le grandprix de F1 du Cananda?", "Lewis Hamilton","Daniel Ricciardo", true),
+            new VraiFaux("Qui a fait a pole position au grandprix de F1 du Cananda?", "Lewis Hamilton","Sebastian Vettel", true),
+            new VraiFaux("Qui a gagné le grandprix de F1 du Cananda?", "Lewis Hamilton","Daniel Ricciardo", true),
+    };
+    private int index = 0;
+private int resultat=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button1 = (Button)findViewById(R.id.button1);
+        final TextView questionText = (TextView) findViewById(R.id.textView2);
+
+
+        final Button button1 = (Button) findViewById(R.id.button1);
+      final Button button2 = (Button) findViewById(R.id.button2);
+
+        questionText.setText(question[index].getQuestion()+resultat);
+        button1.setText(question[index].getQuestion1());
+        button2.setText(question[index].getQuestion2());
+
         button1.setOnClickListener(new View.OnClickListener() {
+
 
             @Override
             public void onClick(View view) {
                 Log.d("Bouton", "Bouton 1");
-                Intent intent = new Intent(MainActivity.this,Activity2.class);
-                intent.putExtra("param", "Vrai ,  et il a réalisé le record de la piste!");
-                startActivity(intent);
+                if(index<question.length-1) {
+                    questionText.setText(question[index].getQuestion()+resultat);
+                    if(question[index].isResponse()){
+                        resultat++;
+                    }
+                    button1.setText(question[index].getQuestion1());
+                    button2.setText(question[index].getQuestion2());
+
+                    index++;
+
+                }
+                else {
+                    Intent intent = new Intent(MainActivity.this, Activity2.class);
+                    final int score = resultat;
+                    intent.putExtra("param",score);
+                    startActivity(intent);
+                }
+                }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View view) {
+                Log.d("Bouton", "Bouton 2");
+
+                if(index<question.length-1) {
+                    questionText.setText(question[index].getQuestion());
+                    if(!question[index].isResponse()){
+                        resultat++;
+                    }
+                    button1.setText(question[index].getQuestion1());
+                    button2.setText(question[index].getQuestion2());
+
+                    index++;
+                }
+                else {
+                    Intent intent = new Intent(MainActivity.this, Activity2.class);
+                    final int score = resultat;
+                    intent.putExtra("param",score);
+                    startActivity(intent);
+                }
             }
         });
-    }
-    public void bouton2(View view){
-        Log.d("Bouton", "Button 2");
-        Intent intent = new Intent(MainActivity.this, Activity2.class);
-        intent.putExtra("param","Faux, il a fait le second chrono");
-        startActivity(intent);
     }
 }
